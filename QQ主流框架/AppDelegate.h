df709e37b61264a64f80d67149fedc3b3120c9f4
Miles <596442958@qq.com>
2016-02-04 06:22:40 +0800

    Initial Commit

---
+++ QQ主流框架/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QQ主流框架
+//
+//  Created by Miles on 2/4/16.
+//  Copyright © 2016 Miles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QQ主流框架/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QQ主流框架
+//
+//  Created by Miles on 2/4/16.
+//  Copyright © 2016 Miles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
